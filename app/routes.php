b478f2534510ccbcb06c6eff8e3b9c82b1b686d8
lpoliveiraneto <leonidespires@gmail.com>
2017-01-10 15:00:14 -0300

    Branch para listar as Sprints em ordem decrescente

--- app/routes.php
+++ app/routes.php
@@ -2,18 +2,32 @@
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use SprintRunServer\Application\Conexao;
+
+
 
 $app = new Silex\Application();
 
-$app->get('sprints', function() {
-  $sprints = [
+$app['debug'] = true;
+
+$conexao = Conexao::getInstance();
+
+
+$app->get('sprints', function() use ($conexao)  {
+  /*$sprints = [
     ['id' => 1, 'nome' => 'Sprint #1', 'obs' => 'Sprint id#1'],
     ['id' => 2, 'nome' => 'Sprint #2', 'obs' => 'Sprint id#2'],
     ['id' => 3, 'nome' => 'Sprint #3', 'obs' => 'Sprint id#3'],
     ['id' => 4, 'nome' => 'Sprint #4', 'obs' => 'Sprint id#4']
-  ];
+  ];*/
+  
+    $stmt = $conexao->prepare('SELECT * FROM sprints ORDER BY id DESC');
+    $stmt->execute();
+    $sprints = $stmt->fetchAll(PDO::FETCH_OBJ);
 
-  return new JsonResponse($sprints, 200);
+    if(!$sprints) return new JsonResponse($sprints, 404);
+
+    return new JsonResponse($sprints, 200);
 });
 
 $app->get('points', function() {
---
+++ src/Conexao.php
@@ -0,0 +1,20 @@
+<?php 
+namespace SprintRunServer\Application;
+
+class Conexao{
+
+	protected static $instancia;
+
+	public static function getInstance(){
+		if(!isset(self::$instancia)){
+			try{
+				self::$instancia = new \PDO('mysql:host=localhost;dbname=Sprints_Points', 'root', 'leonidesNETO', array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+			}catch(PDOException $e){
+				echo 'Erro ao conectar';
+				exit();
+			}
+		}
+
+		return self::$instancia;
+	}
+}
